9352631345a917e73744c4901eb7181f08bcae4b
moosemeow <168628842+thyymoose@users.noreply.github.com>
2024-05-22 19:49:57 +1000

    pole done
    stanley still has not do anything

--- soulthing/Assets/scipts/weapon_man.cs
+++ soulthing/Assets/scipts/weapon_man.cs
@@ -6,13 +6,13 @@ public class weapon_man : MonoBehaviour
 {
     public Behaviour whip;
     public Behaviour lasso;
+    public Behaviour pole;
     // Start is called before the first frame update
     void Start()
     {
         whip.enabled = false;
         lasso.enabled = false;
-        GameObject manger = GameObject.Find("player");
-        lasso_weapon lassofuck = manger.GetComponent<lasso_weapon>();
+        pole.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,27 +22,31 @@ public class weapon_man : MonoBehaviour
         {
             whip.enabled = true;
             lasso.enabled = false;
+            pole.enabled = false;
             GameObject manger = GameObject.Find("player");
-        lasso_weapon lassofuck = manger.GetComponent<lasso_weapon>();
+            lasso_weapon lassofuck = manger.GetComponent<lasso_weapon>();
             lassofuck.fuckinpulled();
         }
         if(Input.GetButtonDown("2"))
         {
             lasso.enabled = true;
             whip.enabled = false;
+            pole.enabled = false;
         }
         if(Input.GetButtonDown("3"))
         {
+            pole.enabled = true;
             whip.enabled = false;
             lasso.enabled = false;
             GameObject manger = GameObject.Find("player");
-        lasso_weapon lassofuck = manger.GetComponent<lasso_weapon>();
+            lasso_weapon lassofuck = manger.GetComponent<lasso_weapon>();
             lassofuck.fuckinpulled();
         }
         if(Input.GetButtonDown("4"))
         {
             whip.enabled = false;
             lasso.enabled = false;
+            pole.enabled = false;
             GameObject manger = GameObject.Find("player");
         lasso_weapon lassofuck = manger.GetComponent<lasso_weapon>();
             lassofuck.fuckinpulled();
---
+++ soulthing/Assets/scipts/weapon_pole.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon_pole : MonoBehaviour
+{
+    public AudioSource src;
+    public AudioClip sfx1;
+    public Transform pullpoint;
+    public LayerMask groundLayer;
+    public Rigidbody2D rb;
+    public Animator ainm;
+    private bool canattak = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!canattak)
+        {
+            return;
+        }
+        if(Input.GetButtonDown("Fire1") && pull_push())
+        {
+         rb.AddForce(new Vector2(rb.velocity.x * 5, 10),ForceMode2D.Impulse);
+         ainm.SetFloat("attackcounter", 4);
+         src.clip = sfx1;
+         src.Play();
+         canattak = false;
+        }
+    }
+    bool pull_push()
+    {
+       return Physics2D.OverlapCircle(pullpoint.position, 0.3f, groundLayer);
+    }
+    public void pole_atackfinish()
+    {
+        ainm.SetFloat("attackcounter",0);
+        canattak = true;        
+    }
+}
